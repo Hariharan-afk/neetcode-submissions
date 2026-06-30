e6f4fd1eec587bc81dd4c4983b553fbfae3eb2d7
Hariharan Chandrasekar <suraj01042003@gmail.com>
2026-06-29 22:37:51 -0400

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        from collections import defaultdict
+
+        hash_map = defaultdict(list)
+        
+        for index,num in enumerate(nums):
+            diff = target - num
+            if diff in hash_map:
+                for j in hash_map[diff]:
+                    if j!=index:
+                        return [min(index, j), max(index, j)]
+            else:
+                hash_map[num].append(index)
+
+        
